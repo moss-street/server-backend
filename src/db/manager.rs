721f8073a32b0efa0474607aec00eaa2bb136c2d
Mershak <mertyapucu2000@gmail.com>
2025-02-09 20:05:25 -0800

    it don't work

--- src/db/manager.rs
+++ src/db/manager.rs
@@ -1,17 +1,25 @@
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
 
-use diesel::query_builder::{InsertStatement, QueryFragment, QueryId};
+use diesel::connection::LoadConnection;
+use diesel::expression::SqlLiteral;
+use diesel::query_builder::{AsQuery, InsertStatement, QueryFragment, QueryId};
+use diesel::query_dsl::methods::{FilterDsl, LoadQuery, LockingDsl};
+use diesel::query_dsl::{methods::BoxedDsl, QueryDsl};
 use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::sql_types::{Bool, Text};
 use diesel::sqlite::SqliteConnection;
 use diesel::{query_dsl::methods::ExecuteDsl, sqlite::Sqlite, Table};
-use diesel::{Insertable, RunQueryDsl};
+use diesel::{Insertable, RunQueryDsl, Queryable};
+use diesel::dsl::sql;
 
 #[async_trait]
 pub trait DatabaseImpl {
-    async fn query_row<T>(&self, fields: Vec<(&str, &str)>) -> Result<T>
+    async fn query_row<'a, T, U>(&self, table: T, fields: Vec<(&str, &str)>) -> Result<Vec<U>>
     where
-        T: Send + Sync + 'static;
+    T: Table + FilterDsl<SqlLiteral<Bool>> + Send + LoadConnection + 'static,
+    U: Queryable<T::SqlType, Sqlite> + Send + Sync + 'static,
+    <T as FilterDsl<SqlLiteral<diesel::sql_types::Bool>>>::Output: Table + diesel::RunQueryDsl<T>+ LoadQuery<'a, T, U>;
 
     async fn query_rows<T>(&self, fields: Vec<(&str, &str)>) -> Result<Vec<T>>
     where
@@ -42,11 +50,37 @@ impl DBManager {
 
 #[async_trait]
 impl DatabaseImpl for DBManager {
-    async fn query_row<T>(&self, _fields: Vec<(&str, &str)>) -> Result<T>
+    async fn query_row<'a, T, U>(&self, table: T, fields: Vec<(&str, &str)>) -> Result<Vec<U>>
     where
-        T: Send + Sync + 'static,
+    T: Table + FilterDsl<SqlLiteral<Bool>> + Send + LoadConnection + 'static,
+    U: Queryable<T::SqlType, Sqlite> + Send + Sync + 'static,
+    <T as FilterDsl<SqlLiteral<diesel::sql_types::Bool>>>::Output: Table + diesel::RunQueryDsl<T>+ LoadQuery<'a, T, U>
     {
-        todo!()
+        let Some(mut conn) = self.connection_pool.try_get() else {
+            return Err(anyhow!("No available connection in connection pool!"));
+        };
+        
+        // Start with an empty filter string
+        let mut filter_string = String::from("");
+
+        // Dynamically build the filter string with AND conditions
+        for (col, val) in fields {
+            // Add each condition as "column = value"
+            let condition = format!("{} = ?", col);
+            filter_string.push_str(" AND ");
+            filter_string.push_str(&condition);
+        }
+
+        // Create the SQL filter expression
+        let query = table.filter(sql::<Bool>(&filter_string));
+
+
+        let results = query  // bind the first parameter as a Text type
+            .load::<U>(&mut *conn)
+            .map_err(|e| anyhow!("Query row error: {e:#?}"))?;
+
+        Ok(results)
+    
     }
     async fn query_rows<T>(&self, _fields: Vec<(&str, &str)>) -> Result<Vec<T>>
     where
